b5453ae4e9b651d0292696ca0b5550482edb9147
Lewis Reid <lewis.reid148@gmail.com>
2020-10-18 13:45:11 +0100

    project start!

---
+++ Controllers/Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace finance_app.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "lol";
+        }
+    }
+}
